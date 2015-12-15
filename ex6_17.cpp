1d775865f415357c081c60f5d3fbf83324e451e2
ZhangLeiStudy <1162973498@qq.com>
2015-12-15 19:51:15 +0800

    Create ex6_17.cpp

---
+++ ex6_17.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+bool flag = false;
+
+string chantToLower(string &s) {
+    string::size_type size = s.size();
+    for(auto i = 0; i < size; ++i)
+    {
+        if(s[i] >= 'A' && s[i] <= 'Z')
+        {
+            flag = true;
+            s[i] = s[i] - ('A' - 'a');
+        }
+    }
+    
+    return s;
+}
+
+int main() 
+{
+    string str("Hello World");
+    string ss = chantToLower(str);
+    
+    cout  << ss << endl;
+    
+    return 0;
+}
