ca6b3d1df954d8b0937313f99f296166806148c9
ZhangLeiStudy <1162973498@qq.com>
2015-12-15 20:36:22 +0800

    Create ex6_21.cpp

---
+++ ex6_21.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int compare(int num, int *ptr)
+{
+    return num >= *ptr ? num : *ptr;
+}
+
+int main(int argc, const char * argv[]) {
+    int num1 = 100, num2 = 110;
+    
+    int larger = compare(num1, &num2);
+    
+    cout << larger << endl;
+    
+    return 0;
+}
