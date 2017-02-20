92f53a3b1e4d3fabad876ee85db57784b9f1e240
ZhangLeiStudy <1162973498@qq.com>
2017-02-20 11:18:25 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 # 从wsgiref模块导入:
 from wsgiref.simple_server import make_server
-# 导入我们自己编写的application函数:
+# 导入pplication函数:
 from hello import application
 
 # 创建一个服务器，IP地址为空，端口是8000，处理函数是application:
